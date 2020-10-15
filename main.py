41c154aea83db9f7aba0c76630c952074e4c71e9
username <54479676+CormacKrum@users.noreply.github.com>
2020-10-15 22:33:55 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -20,11 +20,65 @@ valid_letters = 'qwertyuiopasdfghjklzxcvbnm'
 guessed_word = ['_'] * len(word)
 
 
-def printFigure(guess):
-    pass
+def printFigure(turns):
+    print()
+    if turns == 9:
+        print("9 turns left")
+        print("  --------  ")
+    if turns == 8:
+        print("8 turns left")
+        print("  --------  ")
+        print("     O      ")
+    if turns == 7:
+        print("7 turns left")
+        print("  --------  ")
+        print("     O      ")
+        print("     |      ")
+    if turns == 6:
+        print("6 turns left")
+        print("  --------  ")
+        print("     O      ")
+        print("     |      ")
+        print("    /       ")
+    if turns == 5:
+        print("5 turns left")
+        print("  --------  ")
+        print("     O      ")
+        print("     |      ")
+        print("    / \     ")
+    if turns == 4:
+        print("4 turns left")
+        print("  --------  ")
+        print("   \ O      ")
+        print("     |      ")
+        print("    / \     ")
+    if turns == 3:
+        print("3 turns left")
+        print("  --------  ")
+        print("   \ O /    ")
+        print("     |      ")
+        print("    / \     ")
+    if turns == 2:
+        print("2 turns left")
+        print("  --------  ")
+        print("   \ O /|   ")
+        print("     |      ")
+        print("    / \     ")
+    if turns == 1:
+        print("1 turns left")
+        print("Last breaths counting, Take care!")
+        print("  --------  ")
+        print("   \ O_|/   ")
+        print("     |      ")
+        print("    / \     ")
+    if turns == 0:
+        print("  --------  ")
+        print("     O_|    ")
+        print("    /|\      ")
+        print("    / \     ")
 
 
-while total_guess >= 0:
+while total_guess > 0:
 
     print("Misses : ", end='')
     for i in misses:
@@ -53,4 +107,4 @@ while total_guess >= 0:
         print("Congratulations!! You Guessed It Right\nWord was\033[1m", word, "\033[0m")
         break
 else:
-    print("You Let An Innocent man die.")
+    print("You Loose \nYou Let An Innocent man die.")
